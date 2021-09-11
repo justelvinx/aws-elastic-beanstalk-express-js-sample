c2f7f8835720764f7e187a19e586f227b598ede3
justelvinx <heiselvin@gmail.com>
2021-09-11 10:30:18 +0400

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Heshzad'));
+app.get('/', (req, res) => res.send('Final Commmit'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
